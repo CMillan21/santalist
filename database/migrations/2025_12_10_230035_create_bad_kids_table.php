71a748cb65679c7639a673589091dcc1f9151c24
CMillan21 <christianmanuelmillanpolito9@gmail.com>
2025-12-13 19:19:04 -0600

    Login sin sin OAuth

--- database/migrations/2025_12_10_230035_create_bad_kids_table.php
+++ database/migrations/2025_12_10_230035_create_bad_kids_table.php
@@ -9,19 +9,25 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+
     public function up(): void
     {
-        Schema::create('bad_kids', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table){
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
-        Schema::dropIfExists('bad_kids');
+        Schema::dropIfExists('users');
     }
+
+
 };
